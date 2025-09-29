92a9061689dd14a517fe53a82a63198bf513c79c
Armand Debesse <ardebess@bouyguestelecom.fr>
2025-09-29 15:33:35 +0200

    edit prompts

--- orchestrator/agent.py
+++ orchestrator/agent.py
@@ -5,7 +5,11 @@ from search_agent.agent import root_agent as search_agent
 root_agent = LlmAgent(
     name="orchestrator",
     model="gemini-2.0-flash",
-    description="Agent to answer questions about Versailles Castle.",
-    instruction="Your role is to answer questions about Versailles Castle using the provided tool. Be clear and exhaustive in your answers.",
+    description="Agent to answer questions about Versailles Castle and create itineraries.",
+    instruction="""You are an agent that can answer questions about Versailles Castle and create itineraries for visitors.
+If the user asks a specific question, provide a detailed answer using the search_agent tool.
+If the user wants an itinerary and provides their preferences, create a personalized itinerary using the create_itinerary tool.
+If the user wants an itinerary but does not provide preferences, ask him questions to understand his preferences and situation (age, mobility, interests, time available, budget, children...).
+Answer with the language of the question asked.""",
     tools=[AgentTool(agent=search_agent)]
 )
--- search_agent/agent.py
+++ search_agent/agent.py
@@ -5,7 +5,7 @@ root_agent = LlmAgent(
     name="search_agent",
     model="gemini-2.0-flash",
     description="Agent to answer questions about Versailles Castle.",
-    instruction=["Your role is to answer questions about Versailles Castle using the provided tool. Focus your search on this website : https://www.chateauversailles.fr/",
-                 "Answer with the language of the question asked."],
+    instruction="""Your role is to answer questions about Versailles Castle using the provided tool. Focus your search on this website : https://www.chateauversailles.fr/
+Answer with the language of the question asked.""",
     tools=[google_search]
 )
