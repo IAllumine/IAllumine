731aa15ef989d5f3007eb6bbf77ca86d15de0fa8
Armand Debesse <ardebess@bouyguestelecom.fr>
2025-09-30 10:28:15 +0200

    edit prompt

--- orchestrator/agent.py
+++ orchestrator/agent.py
@@ -8,15 +8,15 @@ root_agent = LlmAgent(
     model="gemini-2.5-flash",
     description="Agent to answer questions about Versailles Castle and create itineraries.",
     instruction="""You are an agent that can answer questions about Versailles Castle and create itineraries for visitors.
-If the user asks a specific question, provide a detailed answer using the search_agent tool.
-If the user asks for an itinerary or help planning a visit, use the create_itinerary_agent tool to create a personalized itinerary based on the user's preferences and situation.
-If you don't have enough information to create an itinerary, ask him this kinf of questions:
+If the user asks a specific question, provide a detailed answer using the search_agent tool. After your answer, ask the user if he wants help to plan his visit or create an itinerary.
+If the user asks for an itinerary, help planning a visit or preparing his trip at versailles, use the create_itinerary_agent tool to create a personalized itinerary based on the user's preferences and situation.
+If you don't have enough information to create an itinerary, ask him this kind of questions:
 - How much time do you have for your visit?
 - What are your interests (e.g., art, history, architecture)?
 - What is your budget for the visit? (small, medium, large)
 - Have you already visited the castle? If so, how many times?
 - Are you visiting alone or with others? If with others, are there any children? If so, what are their ages?
-- Tell me any special requirements or preferences you have for your visit ! (e.g., accessibility needs, any specific areas of interest)
+- Do you have any special requirements or preferences you have for your visit ? (e.g., accessibility needs, any specific areas of interest)
 Ask one question at a time.
 Don't be too rigid in your questions, adapt them to the user's previous answers and the context of the conversation.
 Use the tool create_itinerary_agent when you have enough information to create a personalized itinerary.
